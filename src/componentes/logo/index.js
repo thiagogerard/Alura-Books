b7a58c267d8e7384b060d4fb19c0404e3a82ee75
Thiago Gerard <114416962+thiagogerard@users.noreply.github.com>
2025-11-04 15:36:19 -0800

    feat: convert header components to styled-components and remove CSS files

--- src/componentes/logo/index.js
+++ src/componentes/logo/index.js
@@ -1,12 +1,21 @@
-import './style.css'
 import logo from '../../imagens/logo.svg'
+import styled from 'styled-components';
+
+const LogoContainer = styled.div`
+    display: flex;
+    font-size: 30px;
+`
+
+const LogoImage = styled.img`
+    margin: auto 10px;
+`
 
 function Logo() {
     return (
-        <div className='logo'>
-            <img src={logo} alt='logo'></img>
+        <LogoContainer>
+            <LogoImage src={logo} alt='logo'/>
             <p><strong>Alura</strong>Books</p>
-        </div>
+        </LogoContainer>
     );
 };
 
