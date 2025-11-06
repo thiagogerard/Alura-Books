59a38d8d4d6c85625e55036d4e4d8ff314bc9e6f
Thiago Gerard <114416962+thiagogerard@users.noreply.github.com>
2025-11-05 17:07:53 -0800

    feat: create Titulo component with styled-components for consistent styling

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,4 +1,5 @@
 import { livros } from "./dadosUltimosLancamentos.js";
+import { Titulo } from "../Titulo/index.js";
 import styled from "styled-components";
 
 const UltimosLancamentosContainer = styled.section`
@@ -6,16 +7,6 @@ const UltimosLancamentosContainer = styled.section`
     padding-bottom: 20px;
     display: flex;
     flex-direction: column;
-`
-
-const Titulo = styled.h2`
-    width: 100%;
-    padding: 30px 0;
-    background-color: #FFF;
-    color: #EB9B00;
-    font-size: 36px;
-    text-align: center;
-    margin: 0;
 `
 
 const NovosLivrosContainer = styled.div`
@@ -29,7 +20,7 @@ const NovosLivrosContainer = styled.div`
 function UltimosLancamentos() {
     return (
         <UltimosLancamentosContainer>
-            <Titulo>Últimos Lancamenmtos</Titulo>
+            <Titulo cor='#EB9B00'>Últimos Lancamenmtos</Titulo>
             <NovosLivrosContainer>
                 {livros.map(livro => (
                     <img src={livro.src} alt='livro'/>
